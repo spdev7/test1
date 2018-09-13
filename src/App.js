70f226f177d73a5f0031dc6bd27080bf451d4d56
Sourav Yadav <yadavsourav24071998@gmail.com>
2018-09-13 18:20:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,29 +4,31 @@ import './App.css';
 import Login from './Components/Login';
 import axios from 'axios';
 
-let body;
-let headers;
+
+const headers = {
+    headers:{
+        'Authorization': 'KISI-LOGIN 94c2056abb993b570517f2d3a89c9b5a',
+        'Content-Type':'application/json',
+        'Accept':'application/json',
+    }
+};
+const body = {
+    "user": {
+        "email": "email",
+        "name": "name",
+        "password": "password",
+        "terms_and_conditions": true
+    }
+};
+// let body;
+// let headers;
 class App extends Component {
-     
- body = {
-  "user": {
-    "email": "email",
-    "name": "name",
-    "password": "password",
-    "terms_and_conditions": true
-  }
-};
-headers = {
-  headers:{
-    'Authorization': 'KISI-LOGIN 94c2056abb993b570517f2d3a89c9b5a',
-    'Content-Type':'application/json',
-    'Accept':'application/json',
-  }
-};
+
+
 //https://api.github.com/users/John
 //
 componentDidMount(){
-  axios.get('https://api.getkisi.com/logins?type=type&primary=true&user_id=0',body,headers)
+  axios.post('https://api.getkisi.com/logins?type=type&primary=true&user_id=0',body,headers)
   .then(json => console.log(json))
 }
 constructor(props){
